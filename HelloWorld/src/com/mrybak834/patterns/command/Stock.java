59714c5e95d01c369d8c29f00c82b8b8cc6c1af1
mrybak834 <mrybak834@gmail.com>
2018-09-04 21:16:09 -0500

    Command pattern

---
+++ HelloWorld/src/com/mrybak834/patterns/command/Stock.java
@@ -0,0 +1,11 @@
+package com.mrybak834.patterns.command;
+
+public class Stock {
+    public void buy() {
+        System.out.println("Bought 10 shares");
+    }
+
+    public void sell() {
+        System.out.println("Sold 10 shares");
+    }
+}
